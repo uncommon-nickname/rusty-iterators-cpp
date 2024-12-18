18b57fa66f33b52db5b46e72ba46cdda41d5c7ae
WiktorNowak <wnowak2f@gmail.com>
2024-12-18 07:26:07 +0100

    fix: formatting script

--- format.sh
+++ format.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cxx_files_list="$(cd "${repo_root}" && git ls-tree --full-tree --name-only -r HEAD | grep -E ".*(\.cpp|\.hpp)")"
+cxx_files_list="$(git ls-tree --full-tree --name-only -r HEAD | grep -E ".*(\.cpp|\.hpp)")"
 clang-format --verbose -i $cxx_files_list
